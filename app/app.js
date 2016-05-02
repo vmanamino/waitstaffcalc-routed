43a0a17a134dc2a5dcfc3e8903814fe4a7417a2e
vmanamino <vmanamino@gmail.com>
2016-05-02 22:57:01 +0000

    errors routed

--- app/app.js
+++ app/app.js
@@ -2,17 +2,34 @@ angular.module('myApp', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl : 'index.html',
-                controller : 'IndexCtrl'
+                templateUrl : 'home.html',
+                controller : 'HomeCtrl'
             })
             .when('/details', {
                 templateUrl : 'details.html',
                 controller : 'DetailsCtrl'
-            });
+            })
+            .when('/earnings', {
+                templateUrl : 'earnings.html',
+                controller : 'EarnCtrl'
+            })
+            .when('/error', {
+                template : '<p>Error - Page Not Found</p>'
+            })
+            .otherwise('/error');
     }])
-    .controller('IndexCtrl', ['$scope', function($scope) {
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+        'use strict';
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path('/error');
+        });
+    }])
+    .controller('HomeCtrl', ['$scope', function($scope) {
         
     } ])
     .controller('DetailsCtrl', ['$scope', function($scope) {
     
+    }])
+    .controller('EarnCtrl', ['$scope', function($scope) {
+        
     }]);
