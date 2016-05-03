5320de13427c562af061871308ebc7ea8f5d38ee
vmanamino <vmanamino@gmail.com>
2016-05-03 04:21:20 +0000

    need to build service

--- app/app.js
+++ app/app.js
@@ -24,12 +24,56 @@ angular.module('myApp', ['ngRoute'])
             $location.path('/error');
         });
     }])
-    .controller('HomeCtrl', ['$scope', function($scope) {
+    .controller('HomeCtrl', ['$rootScope', function($rootScope) {
         
     } ])
-    .controller('DetailsCtrl', ['$scope', function($scope) {
-    
+    .controller('DetailsCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $rootScope.basemeal = '';
+        $scope.taxrate = '';
+        $scope.tipPercentage = '';
+        $scope.tip =  0;
+        $scope.subtotal = '';
+        $scope.total = '';
+        $rootScope.tiptotal = 0;
+        $rootScope.meals = 0;
+        $rootScope.avg = '';
+        $scope.displayErrors = 'no';
+        
+        $scope.submit = function(){
+            console.log($rootScope.basemeal);
+            if ($rootScope.basemeal && $scope.taxrate && $scope.tipPercentage){
+                $scope.subtotal = $rootScope.basemeal + ($rootScope.basemeal * ($scope.taxrate / 100));
+                $scope.tip = $rootScope.basemeal * ($scope.tipPercentage / 100);
+                $scope.total = $scope.subtotal + $scope.tip;
+                addUp($scope.tip);
+                
+            }
+            else {
+                $scope.displayErrors = '';
+            }
+        };
+        $scope.cancel = function(){
+            $rootScope.basemeal = '';
+            $scope.taxrate = '';
+            $scope.tipPercentage = '';
+        }
+        var addUp = function(tip){
+            $rootScope.meals += 1;
+            $rootScope.tiptotal += tip;
+            $rootScope.avg = $rootScope.tiptotal / $rootScope.meals
+            
+            
+        };
+
     }])
-    .controller('EarnCtrl', ['$scope', function($scope) {
+    .controller('EarnCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
+        $scope.meals = $rootScope.meals;
+        $scope.tiptotal = $rootScope.tiptotal;
+        $scope.avg = $rootScope.avg;
+        
+        $scope.reset = function(){
+            $rootScope.basemeal = 0;
+            console.log($rootScope.basemeal);
+        };
         
     }]);
